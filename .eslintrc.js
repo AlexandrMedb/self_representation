74a8bbe26c708dbac3a08e49d7c6a8e5401f8ed6
Alexandr <torasov.alexandr@gmail.com>
2022-10-02 19:18:33 +0300

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,56 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  extends: ["plugin:react/recommended", "google", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-    'simple-import-sort',
-  ],
-  'rules': {
-    'linebreak-style': 0,
-    'react/prop-types': 0,
-    'require-jsdoc': 0,
-    'max-len': [
+  plugins: ["react", "@typescript-eslint", "simple-import-sort"],
+  rules: {
+    "linebreak-style": 0,
+    "react/prop-types": 0,
+    "require-jsdoc": 0,
+    "max-len": [
       2,
       {
         code: 100,
         ignoreComments: true,
       },
     ],
-    'react/display-name': 1,
-    'react/no-unescaped-entities': 1,
-    'no-unused-vars': 1,
-    'valid-jsdoc': 1,
-    'camelcase': 'off',
-    'no-console': 1,
-    'simple-import-sort/imports': [
+    "react/display-name": 1,
+    "react/no-unescaped-entities": 1,
+    "no-unused-vars": 1,
+    "valid-jsdoc": 1,
+    camelcase: "off",
+    "no-console": 1,
+    "simple-import-sort/imports": [
       1,
       {
         groups: [
           // `react` first, `next` second, then packages starting with a character
-          ['^react$', '^next', '^[a-z]'],
+          ["^react$", "^next", "^[a-z]"],
           // Packages starting with `@`
-          ['^@'],
+          ["^@"],
           // Packages starting with `~`
-          ['^~'],
+          ["^~"],
           // Imports starting with `../`
-          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
           // Imports starting with `./`
-          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
           // Style imports
-          ['^.+\\.s?css$'],
+          ["^.+\\.s?css$"],
           // Side effect imports
-          ['^\\u0000'],
+          ["^\\u0000"],
         ],
       },
     ],
-    'simple-import-sort/exports': 'error',
+    "simple-import-sort/exports": "error",
   },
 };
